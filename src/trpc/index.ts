6f35c66c56fcf85453a5024affec4d5f8371bc14
DracoR22 <rdraco039@gmail.com>
2024-01-12 21:54:28 -0500

    stripe implementation

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,10 +1,12 @@
 import { authRouter } from "./routers/auth-router";
+import { paymentRouter } from "./routers/payment-router";
 import { ProductRouter } from "./routers/product-router";
 import { router } from "./trpc";
 
 export const appRouter = router({
    auth: authRouter,
-   products: ProductRouter
+   products: ProductRouter,
+   payment: paymentRouter
 })
 
 export type AppRouter = typeof appRouter
